ae8eb9e92786ebb00ee645103ebbba9f25f7a12b
Matthew Sowden <zxa243@gmail.com>
2012-07-30 23:19:45 -0400

    Automated stat banner images. BECAUSE EVERYONE NEEDS THEM TO BE SUCCESSFULL. EVERYONE.

--- refill/apps/plugin/views.py
+++ refill/apps/plugin/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+from django.http import HttpResponse
+import Image, ImageDraw
+
+__root_image = Image.new("RGB", (1,1))
+__sample_canvas = ImageDraw.Draw(__root_image)
+
+def text_image(request, display_string):
+    global __sample_canvas
+    response = HttpResponse(mimetype="image/png")
+
+    x, y = __sample_canvas.textsize(display_string)
+    image = Image.new("RGB", (x+10, y))
+    canvas = ImageDraw.Draw(image)
+    canvas.text((5,0), display_string)
+
+    image.rotate(90).save(response, "PNG")
+    return response
